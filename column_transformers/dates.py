d7a5f6fd564c64b60bfb8da39e06820d7e47a543
Joseph <j.moorhouse1898@gmail.com>
2019-09-10 16:08:36 +0100

    Added dates.py and date features

---
+++ column_transformers/dates.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.base import TransformerMixin
+
+class DateDummy(TransformerMixin):
+    def __init__(self, *args):
+        self.args = args
+
+    def fit(self, X, y=None):
+        return self
+    
+    def _get_dummy_variables(self, X):
+        for arg in self.args:
+            date_feature = getattr(X.index.to_series().dt, arg)
+            
+            if callable(date_feature):
+                date_feature = date_feature()
+                
+            yield pd.get_dummies(date_feature)
+    
+    def transform(self, X, y=None):
+        dummy_df = list(self._get_dummy_variables(X))
+        
+        return pd.concat([X, *dummy_df], axis = 1)
