2b6bba04e6b79e0637184d4f1942ddb55436ab84
Joseph <j.moorhouse1898@gmail.com>
2019-09-10 12:15:46 +0100

    Added TrainValTest module

--- column_transformers/technical_indicators.py
+++ column_transformers/technical_indicators.py
@@ -20,7 +20,7 @@ class StochRsiSignal(BaseEstimator, TransformerMixin):
         return self
     
     def transform(self, X, y = None):
-        stochrsi_fastd = X.loc[:, "stochrsi_fastd"]
+        stochrsi_fastd = X["stochrsi_fastd"]
         
         stochrsi_fastd_buy = (
             (stochrsi_fastd >= self.lower) & 
@@ -39,3 +39,41 @@ class StochRsiSignal(BaseEstimator, TransformerMixin):
             return X
         else:
             return np.c_[X, stochrsi_fastd_buy, stochrsi_fastd_sell]
+        
+        
+        
+class MacdSignal(BaseEstimator, TransformerMixin):
+    
+    # ----------------------------------------------------------------------
+    # Constructors
+    
+    def __init__(self, scale=1, lower=-40, upper=40, as_dataframe = True):
+        self.scale = scale
+        self.lower = lower * scale
+        self.upper = upper * scale
+        self.as_dataframe = as_dataframe
+        
+    # ----------------------------------------------------------------------
+    # fit, transform methods 
+    
+    def fit(self, X, y=None):
+        return self
+    
+    
+    def transform(self, X, y=None):
+        macd_fast = X['macd']
+        macd_slow = X['macd_macdsignal']
+        
+        uptrend = (macd_fast > macd_slow).astype(int)
+    
+        #macd_fast = np.ceil(macd_fast * self.scale)
+        macd_fast.clip(self.lower, self.upper, inplace = True)
+        
+        if self.as_dataframe:
+            return X.assign(
+                macd_trend_signal = uptrend * macd_fast,
+                macd_uptrend = uptrend
+            )
+        else:
+            return np.c_[X, uptrend * macd_fast, uptrend]
+        
---
+++ split/_split.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import numpy as np
+
+class TrainValidateTest():
+    """
+    train_size, 
+    valid_size, 
+    test_size
+    """
+    
+    # ----------------------------------------------------------------------
+    # Constructors
+    
+    def __init__(self, train_size, valid_size, test_size):
+        
+        if self._check_data_sizes(train_size, valid_size, test_size):
+            raise ValueError (
+            "Ensure that the train_size, valid_size and test_size are "
+            "individually less than 1, greater than 0 and sum to 1.")
+        
+        self.train_size = train_size
+        self.valid_size = valid_size
+        self.test_size = test_size
+        
+        
+    def _check_data_sizes(self, *args):
+        data = np.array(args)
+        return ((data < 0) | (data > 1) | (data.sum() != 1)).any()
+    
+    # ----------------------------------------------------------------------
+    # fit, transform methods
+
+    def transform(self, X, y = None):
+        """
+        Generate the train, validation, and test dataset.
+
+        Parameters
+        ----------
+        all_x : DataFrame
+            All the input samples
+        all_y : Pandas Series
+            All the target values
+
+        Returns
+        -------
+        x_train : DataFrame
+            The train input samples
+        x_valid : DataFrame
+            The validation input samples
+        x_test : DataFrame
+            The test input samples
+        y_train : Pandas Series
+            The train target values
+        y_valid : Pandas Series
+            The validation target values
+        y_test : Pandas Series
+            The test target values
+        """
+
+        dates = X.index
+        train_size, valid_size, test_size = (
+            self.train_size, self.valid_size, self.test_size
+        )
+
+        train_dates, valid_dates, test_dates = np.split(
+            dates, [int(len(dates) * train_size), 
+                    int(len(dates) * (train_size + test_size))]
+        )
+
+        return (
+            X.loc[train_dates, :],
+            X.loc[valid_dates, :],
+            X.loc[test_dates, :],
+            y.loc[train_dates],
+            y.loc[valid_dates],
+            y.loc[test_dates])
