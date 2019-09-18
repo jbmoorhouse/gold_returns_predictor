94a96a19a2e98910c10e8a6aba599d71e12cfb3b
Joseph <j.moorhouse1898@gmail.com>
2019-09-18 18:53:02 +0100

    Added fit method to MacdStrategy

--- column_transformers/alpha_factors.py
+++ column_transformers/alpha_factors.py
@@ -32,26 +32,47 @@ class MacdStrategy(BaseEstimator, TransformerMixin):
     
     """
     
-    # ----------------------------------------------------------------------
+    # ======================================================================
     # Constructors
+    # ======================================================================
 
     def __init__(self, fast_period=12, slow_period=26, signal_period=9):
+        
+        # set default model parameters if user wishes to use self.transform
+        # with experimental parameters
         self.fast_period = fast_period
         self.slow_period = slow_period
         self.signal_period = signal_period
         
+        # set reasonable test time constants
+        self.fast_period_test = np.arange(1,20)
+        self.slow_period_test = np.arange(2, 30)
+        self.fast_period_test = np.arange(2, 15)
         
-    # ----------------------------------------------------------------------
+        # cost
+        self.ratio = ratio
+        
+        
+    # ======================================================================
     # Rendering Methods
+    # ======================================================================
+
         
     def __repr__(self):
         return "TBD"
     
     
-    # ----------------------------------------------------------------------
+    # ======================================================================
     # strategy statistics/signals
+    # ======================================================================
     
-    def _macd(self, X, column_name='price'):
+    def _macd(self, 
+              fast_period = None, 
+              slow_period = None, 
+              signal_period = None
+              X = None, 
+              column_name='price', 
+              ):
         """
         Two-dimensional tabular data structure containing a price series and 
         claculated macd time series.
@@ -72,16 +93,31 @@ class MacdStrategy(BaseEstimator, TransformerMixin):
           
         """
         
+        # determine if default macd quantities shoud be used
+        if fast_period is None:
+            fast_period = self.fast_period
+        if slow_period is None:
+            slow_period = self.slow_period
+        if signal_period is None:
+            signal_period = self.signal_period
+            
+    
         if not isinstance(column_name, str):
             raise TypeError("'column_name' must be of type str")
 
+            
         if not isinstance(X, (pd.DataFrame, pd.Series)):
             raise TypeError("'X' must be of type pd.DataFrame or pd.Series")            
         elif isinstance(X, pd.DataFrame):
             
             # calculate macd time series
             price_series = X[column_name]
-            macd, macd_signal, macd_hist = talib.MACD(price_series.values)
+            macd, macd_signal, macd_hist = talib.MACD(
+                price_series.values,
+                fastperiod = self.fast_period,
+                slowperiod = self.slow_period
+                signalperiod = self.signal_period
+            )
 
             return X.assign(
                 macd = macd,
@@ -90,37 +126,91 @@ class MacdStrategy(BaseEstimator, TransformerMixin):
             )
         elif isinstance(X, pd.Series):
             # calculate the macd time series
-            macd, macd_signal, macd_hist = talib.MACD(X.values)
+            macd, macd_signal, macd_hist = talib.MACD(
+                X.values,
+                fastperiod = self.fast_period,
+                slowperiod = self.slow_period
+                signalperiod = self.signal_period
+            )
             
             return pd.DataFrame(
-                data=np.c_[X, macd, macd_signal, macd_hist], 
-                index=X.index, 
-                columns=[column_name, 'macd', 'macd_signal', 'macd_hist'])
-
+                data = np.c_[X, macd, macd_signal, macd_hist], 
+                index = X.index, 
+                columns = [column_name, 'macd', 'macd_signal', 'macd_hist'])
             
             
     def _long_signal(self, X):
         self.long = (X['macd'].shift(1) > X['macd_signal'].shift(1))
-        #reversal = ((X['macd'].diff().shift(1) > 0) & (df['macd'].diff().shift(2) > 0))
+        #reversal = ((X['macd'].diff().shift(1) > 0) & 
+        #(df['macd'].diff().shift(2) > 0))
         
         return (self.long) * 1
     
     
     def _short_signal(self, X):
         self.short = (X['macd'].shift(1) < X['macd_signal'].shift(1)) 
-        #reversal = ((df['macd'].diff().shift(1) < 0) & (df['macd'].diff().shift(2) < 0))
+        #reversal = ((df['macd'].diff().shift(1) < 0) & 
+        #(df['macd'].diff().shift(2) < 0))
                 
         return (self.short) * -1
+    
+    
+    def _strategy_returns(self, X):
         
+        # get MACD oscillator statistics
+        X_macd = self._macd(X = X)
         
+        # generate strategy long/short signal and asset returns
+        long_signal = self._long_signal(X_macd)
+        short_signal = self._short_signal(X_macd)
+        
+        # calculate the assets simple returns 
+        asset_returns = X_macd['price'].pct_change()
+        
+        return (asset_returns * long_signal) + (asset_returns * short_signal)
+
+   
+    # ======================================================================
+    # Parameter estimation
+    # ======================================================================
+
+    
+    def _cost_function(self, returns, annualisation_factor, risk_free_rate):
+        
+        excess_return = (
+            (np.sqrt(annualisation_factor) * returns.mean()) - risk_free_rate
+        )
+        
+        return excess_return / returns.std()
+    
+    
+    def _fit(self, X, y, annualisation_factor, risk_free_rate):
+        
+        for f in self.fast_period_test:
+            for s in self.slow_period_test:
+                for p in self.signal_period:
+                    
+                    strategy_returns = self._strategy_returns(X)
+                    ratio = _cost_function(
+                        strategy_returns,
+                        annualisation_factor
+                    )
+                    
+                    if ratio > self.ratio:
+                        self.fast_period = f
+                        self.slow_period = s
+                        self.signal_period = p
         
     # ======================================================================
     # Public methods
     # ======================================================================
     
     
-    def fit(self, X, y=None):
-        return self
+    def fit(self, X, y=None, annualisation_factor=252, risk_free_rate=0.01):
+        
+        self._fit(X, y, annualisation_factor, risk_free_rate)
+        
+        return self 
         
         
     def transform(self, X, y=None):
@@ -170,19 +260,9 @@ class MacdStrategy(BaseEstimator, TransformerMixin):
         
         """
         
-        # get MACD oscillator statistics
-        X_macd = self._macd(X)
-        
-        # generate strategy long/short signal and asset returns
-        long_signal = self._long_signal(X_macd)
-        short_signal = self._short_signal(X_macd)
-        
-        # calculate the assets simple returns 
-        asset_returns = X_macd['price'].pct_change()
-        
         # add strategy return column
-        X_macd.loc[:, 'macd_strategy_returns'] = (
-            (asset_returns * long_signal) + (asset_returns * short_signal)
+        X.loc[:, 'macd_strategy_returns'] = (
+            self._strategy_returns(X)
         )
         
-        return X_macd
+        return X
