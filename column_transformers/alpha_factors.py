1f974a54f737a8a67e68f0a7af71860bb733b350
Joseph <j.moorhouse1898@gmail.com>
2019-09-20 16:25:03 +0100

    Updated MacdStrategy

--- column_transformers/alpha_factors.py
+++ column_transformers/alpha_factors.py
@@ -19,6 +19,7 @@ class MacdStrategy(BaseEstimator, TransformerMixin):
     
     Parameters
     ----------
+    result : str or int, default 'optimal'
     fast_period : float, default 12.
         Time constant for the 'fast' price series exponential moving average
     slow_period : float, default 26.
@@ -35,6 +36,12 @@ class MacdStrategy(BaseEstimator, TransformerMixin):
     """
     
     # ======================================================================
+    # Constants
+    # ======================================================================
+    
+    PRICE, MACD, MACD_SIGNAL, MACD_HIST, RATIO = 0, 1, 2, 3, 3
+    
+    # ======================================================================
     # Constructors
     # ======================================================================
 
@@ -43,6 +50,17 @@ class MacdStrategy(BaseEstimator, TransformerMixin):
                  fast_period=12.0, 
                  slow_period=26.0, 
                  signal_period=9.0):
+        
+        
+        if result == 'optimal':
+            self.optimal_result = True
+            self.optimal_parameters = 0
+        elif isinstance(result, (int, float)):
+            self.optimal_result = False
+            self.optimal_parameters = np.zeros([int(result), 4])
+        else:
+            raise ValueError(
+                "'result' must be either a valid string or an int/float ")
         
         # set default model parameters if user wishes to use self.transform
         # with experimental parameters
@@ -55,8 +73,7 @@ class MacdStrategy(BaseEstimator, TransformerMixin):
         self.slow_period_test = np.arange(3, 30, dtype = float)
         self.signal_period_test = np.arange(4, 15, dtype = float)
         
-        # cost
-        self.ratio = 0
+        
         
         
     # ======================================================================
@@ -72,13 +89,12 @@ class MacdStrategy(BaseEstimator, TransformerMixin):
     # strategy statistics/signals
     # ======================================================================
     
-    def _macd(self, 
+    def _macd(self,
+              X = None,
               fast_period = None, 
               slow_period = None, 
-              signal_period = None,
-              X = None, 
-              column_name='price', 
-              ):
+              signal_period = None, 
+              column_name='price'):
         """
         Two-dimensional tabular data structure containing a price series and 
         claculated macd time series.
@@ -107,7 +123,7 @@ class MacdStrategy(BaseEstimator, TransformerMixin):
         if signal_period is None:
             signal_period = self.signal_period
             
-    
+            
         if not isinstance(column_name, str):
             raise TypeError("'column_name' must be of type str")
 
@@ -115,62 +131,49 @@ class MacdStrategy(BaseEstimator, TransformerMixin):
         if not isinstance(X, (pd.DataFrame, pd.Series)):
             raise TypeError("'X' must be of type pd.DataFrame or pd.Series")            
         elif isinstance(X, pd.DataFrame):
-            
-            # calculate macd time series
-            price_series = X[column_name]
-            
-            macd, macd_signal, macd_hist = talib.MACD(
-                price_series.values,
-                fastperiod = fast_period,
-                slowperiod = slow_period,
-                signalperiod = signal_period
-            )
-
-            return X.assign(
-                macd = macd,
-                macd_signal = macd_signal,
-                macdhist = macd_hist
-            )
+            price_series = X[column_name].values
         elif isinstance(X, pd.Series):
+            price_series = X.values
+            
+        # returns a tuple of macd, macd_signal and mach_hist
+        macd_statistics = talib.MACD(
+            price_series,
+            fastperiod = fast_period,
+            slowperiod = slow_period,
+            signalperiod = signal_period
+        )    
         
-            # calculate the macd time series
-            macd, macd_signal, macd_hist = talib.MACD(
-                X.values,
-                fastperiod = fast_period,
-                slowperiod = slow_period,
-                signalperiod = signal_period
-            )
-            
-            return pd.DataFrame(
-                data = np.c_[X, macd, macd_signal, macd_hist], 
-                index = X.index, 
-                columns = [column_name, 'macd', 'macd_signal', 'macd_hist'])
-            
-            
-    def _long_signal(self, X):
-        self.long = (X['macd'].shift(1) > X['macd_signal'].shift(1))
-        #reversal = ((X['macd'].diff().shift(1) > 0) & 
-        #(df['macd'].diff().shift(2) > 0))
+        price_macd = np.c_[price_series, np.array(macd_statistics).T]
         
-        return (self.long) * 1
+        return price_macd[~np.isnan(price_macd).any(1), :]
+
+            
+    def _long_signal(self, price_macd):
+        long = price_macd[:, self.MACD] > price_macd[:, self.MACD_SIGNAL]
+        
+        return long[:-1] * 1
     
     
-    def _short_signal(self, X):
-        self.short = (X['macd'].shift(1) < X['macd_signal'].shift(1)) 
-        #reversal = ((df['macd'].diff().shift(1) < 0) & 
-        #(df['macd'].diff().shift(2) < 0))
-                
-        return (self.short) * -1
+    def _short_signal(self, price_macd):
+        short = price_macd[:, self.MACD] < price_macd[:, self.MACD_SIGNAL]
+       
+        return short[:-1] * -1
     
     
-    def _strategy_returns(self, X_macd):
+    def _asset_returns(self, price_macd):
+        return (
+            np.diff(price_macd[:, self.PRICE]) / price_macd[:-1, self.PRICE]
+        )
+    
+    
+    def _strategy_returns(self, price_macd):
         
         # generate strategy long/short signal and asset returns
-        long_signal = self._long_signal(X_macd)
-        short_signal = self._short_signal(X_macd)
+        long_signal = self._long_signal(price_macd)
+        short_signal = self._short_signal(price_macd)
         
         # calculate the assets simple returns 
-        asset_returns = X_macd['price'].pct_change()
+        asset_returns = self._asset_returns(price_macd)
         
         return (asset_returns * long_signal) + (asset_returns * short_signal)
 
@@ -180,10 +183,13 @@ class MacdStrategy(BaseEstimator, TransformerMixin):
     # ======================================================================
 
     
-    def _cost_function(self, returns, annualisation_factor, risk_free_rate):
+    def _cost_function(self, 
+                       strategy_returns, 
+                       annualisation_factor, 
+                       risk_free_rate):
         
         excess_return = (
-            (np.sqrt(annualisation_factor) * returns.mean()) - risk_free_rate
+            (np.sqrt(annualisation_factor) * strategy_returns.mean()) - risk_free_rate
         )
         
         return excess_return / returns.std()
@@ -195,19 +201,28 @@ class MacdStrategy(BaseEstimator, TransformerMixin):
             for s in self.slow_period_test:
                 for p in self.signal_period_test:
                     
-                    X_macd = self._macd(f, s, p, X)
-                    strategy_returns = self._strategy_returns(X_macd)
+                    price_macd = self._macd(X, f, s, p)
+                    strategy_returns = self._strategy_returns(price_macd)
+                    
                     ratio = self._cost_function(
-                        strategy_returns,
-                        annualisation_factor,
-                        risk_free_rate
-                    )
-                                    
-                    if ratio > self.ratio:
-                        self.ratio = ratio
-                        self.fast_period = f
-                        self.slow_period = s
-                        self.signal_period = p
+                            strategy_returns,
+                            annualisation_factor,
+                            risk_free_rate
+                        )
+                    
+                    if self.optimal_result:
+                        if ratio > self.optimal_ratio:
+                            self.ratio = ratio
+                            self.fast_period = f
+                            self.slow_period = s
+                            self.signal_period = p
+                    else:
+                        argmin = self.optimal_parameters[:, self.RATIO].argmin()
+                        
+                        if ratio > self.optimal_parameters[argmin, self.RATIO]:
+                            self.optimal_parameters[argmin] = [
+                                f, s, p, ratio
+                            ]
                         
         
     # ======================================================================
@@ -216,11 +231,12 @@ class MacdStrategy(BaseEstimator, TransformerMixin):
     
     
     def fit(self, X, y=None, annualisation_factor=252, risk_free_rate=0.00):
+        
         self._fit(X, y, annualisation_factor, risk_free_rate)
         return self 
         
         
-    def transform(self, X, y=None):
+    def transform(self, X, y=None, as_frame=False):
         """
         Transform the asset price series into a return time series using the 
         predefined macd momentum based strategy. 
@@ -267,10 +283,16 @@ class MacdStrategy(BaseEstimator, TransformerMixin):
         
         """
         
-        X_macd = self._macd(X = X)
+        price_macd = self._macd(X)
         
-        X_macd['macd_strategy_returns'] = (
-            self._strategy_returns(X_macd)
+        index_size = X.index.size
+        new_index = X.index[(index_size) - price_macd.shape[0]:]
+        
+        price_macd_df = pd.DataFrame(price_macd, index = new_index)
+        asset_returns = np.insert(self._strategy_returns(price_macd), 0, np.nan)
+      
+        price_macd_df['macd_strategy_returns'] = (
+            asset_returns
         )
         
-        return X_macd
+        return price_macd_df
